eaf30aeaa10656e7d6bba036bfc48aa25f8be8f1
Denislav883 <yanev.denislav@gmail.com>
2026-03-03 23:21:33 +0200

    Add Recipe Mongoose model
    Create a Recipe Mongoose schema and model with required fields: title, ingredients, instructions, time, and image. Exports the default Recipe model for use in the server application to persist recipe data.

---
+++ server/models/Recipe.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const recipeSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    ingredients: {
+        type: String,
+        required: true
+    },
+    instructions: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+});
+
+const Recipe = model("Recipe", recipeSchema);
+
+export default Recipe
