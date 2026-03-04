e012c1a09c787f136dea8160ba48e04f5a6684bc
Denislav883 <yanev.denislav@gmail.com>
2026-03-04 18:58:04 +0200

    Add User model and bcrypt dependency
    Create a Mongoose User model (server/models/User.js) with email and password fields and a pre-save hook that hashes passwords using bcrypt (12 salt rounds).

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, "User email is required!"],
+        unique: [true, "Email should be unique!"],
+        minLength: [6, "Email should be at least 6 characters long!"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required!"],
+        minLength: [6, "Password should be at least 6 characters long!"]
+    }
+});
+
+userSchema.pre("save", async function() {
+
+    this.password = await bcrypt.hash(this.password, 12); // generate authomatically salt 
+});
+
+const User = model("User", userSchema);
+
+export default User;    
